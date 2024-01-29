2f738fa882421291f2f2f53a57e63220de97f2a4
Гагара Николай Федорович <ggn.f764@gmail.com>
2024-01-29 12:14:06 +0300

    добавил переменные цветов

--- App.tsx
+++ App.tsx
@@ -1,30 +1,35 @@
 import React, { useState } from "react";
 import * as eva from "@eva-design/eva";
-import {
-    ApplicationProvider,
-    Layout,
-    Text,
-    Datepicker,
-} from "@ui-kitten/components";
+import { ApplicationProvider, Layout, Text, I18nConfig, NativeDateService, RangeCalendar, Spinner } from "@ui-kitten/components";
+import { default as theme } from "./custom-theme.json";
 
-const HomeScreen = () => {
+const i18n: I18nConfig = {
+    dayNames: {
+        short: ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"],
+        long: ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"],
+    },
+    monthNames: {
+        short: ["Янв", "Фев", "Март", "Апр", "Май", "Июнь", "Июль", "Авг", "Сен", "Окт", "Ноя", "Дек"],
+        long: ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"],
+    },
+};
+
+const localeDateService = new NativeDateService("ru", { i18n, startDayOfWeek: 1 });
+
+const TimetableScreen = () => {
     const [date, setDate] = useState(new Date());
+    const [range, setRange] = React.useState({});
 
     return (
-        <Layout
-            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-        >
-            <Text category="h1">HOME</Text>
-            <Datepicker
-                date={date}
-                onSelect={(nextDate) => setDate(nextDate)}
-            />
+        <Layout style={{ flex: 1, alignItems: "center" }}>
+            <RangeCalendar range={range} onSelect={(nextRange) => setRange(nextRange)} dateService={localeDateService} />
+            <Spinner status="primary" />
         </Layout>
     );
 };
 
 export default () => (
-    <ApplicationProvider {...eva} theme={eva.light}>
-        <HomeScreen />
+    <ApplicationProvider {...eva} theme={{ ...eva.light, ...theme }}>
+        <TimetableScreen />
     </ApplicationProvider>
 );
