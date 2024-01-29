189b0954d426ac680eb834e7af6b383cf9d47c15
Гагара Николай Федорович <ggn.f764@gmail.com>
2024-01-29 12:55:42 +0300

    тест вёрстки

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import { ImageBackground, StyleSheet } from "react-native";
 import * as eva from "@eva-design/eva";
-import { ApplicationProvider, Layout, Text, I18nConfig, NativeDateService, RangeCalendar, Spinner } from "@ui-kitten/components";
+import { ApplicationProvider, Text, I18nConfig, NativeDateService, RangeCalendar, Spinner } from "@ui-kitten/components";
 import { default as theme } from "./custom-theme.json";
 
 const i18n: I18nConfig = {
@@ -16,15 +17,23 @@ const i18n: I18nConfig = {
 
 const localeDateService = new NativeDateService("ru", { i18n, startDayOfWeek: 1 });
 
+const bg = { uri: "./assets/bg.png" };
+
 const TimetableScreen = () => {
     const [date, setDate] = useState(new Date());
     const [range, setRange] = React.useState({});
 
     return (
-        <Layout style={{ flex: 1, alignItems: "center" }}>
-            <RangeCalendar range={range} onSelect={(nextRange) => setRange(nextRange)} dateService={localeDateService} />
-            <Spinner status="primary" />
-        </Layout>
+        <ImageBackground source={bg} resizeMode="cover" style={styles.image}>
+            <Text status="warning">Success</Text>
+            <RangeCalendar
+                style={{ width: "100%", borderColor: "background: rgba(0,0,0, 0)" }}
+                range={range}
+                onSelect={(nextRange) => setRange(nextRange)}
+                dateService={localeDateService}
+            />
+            <Spinner status="warning" />
+        </ImageBackground>
     );
 };
 
@@ -33,3 +42,22 @@ export default () => (
         <TimetableScreen />
     </ApplicationProvider>
 );
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: "100%",
+    },
+    image: {
+        flex: 1,
+        justifyContent: "center",
+    },
+    text: {
+        color: "white",
+        fontSize: 42,
+        lineHeight: 84,
+        fontWeight: "bold",
+        textAlign: "center",
+        backgroundColor: "#000000c0",
+    },
+});
