177263ee7a7567dd83f79f533b6b3bc472e4750c
turboreaktivnayatyaga <ggn.f764@gmail.com>
2024-01-31 22:05:56 +0300

    правка стилей переменных, чуть потрогал шапку

--- components/Calendar/Calendar.tsx
+++ components/Calendar/Calendar.tsx
@@ -1,49 +1,20 @@
 import { useState } from "react";
-import { Text, I18nConfig, NativeDateService, RangeCalendar } from "@ui-kitten/components";
-import { View, StyleSheet } from "react-native";
+import { NativeDateService, RangeCalendar } from "@ui-kitten/components";
+import { i18n } from './i18n';
 
-const i18n: I18nConfig = {
-    dayNames: {
-        short: ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"],
-        long: ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"],
-    },
-    monthNames: {
-        short: ["Янв", "Фев", "Март", "Апр", "Май", "Июнь", "Июль", "Авг", "Сен", "Окт", "Ноя", "Дек"],
-        long: ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"],
-    },
-};
-
-const localeDateService = new NativeDateService("ru", { i18n, startDayOfWeek: 1 });
+const localeDateService = new NativeDateService("ru", {
+    i18n,
+    startDayOfWeek: 1,
+});
 
 export const Calendar = () => {
     const [range, setRange] = useState({});
 
-    const renderDay = (date: any, info: any) => {
-        // console.log(date.date);
-        console.log(range.startDate);
-        console.log(range.endDate);
-
-        const style = info.selected ? styles.selectedDay : styles.defaultDay;
-        return (
-            <View style={style}>
-                <Text>{99}</Text>
-            </View>
-        );
-    };
-
-    return <RangeCalendar range={range} renderDay={renderDay} onSelect={(nextRange) => setRange(nextRange)} dateService={localeDateService} />;
+    return (
+        <RangeCalendar
+            range={range}
+            onSelect={(nextRange) => setRange(nextRange)}
+            dateService={localeDateService}
+        />
+    );
 };
-
-const styles = StyleSheet.create({
-    defaultDay: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    selectedDay: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "green",
-    },
-});
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,17 +2,27 @@ import React from "react";
 import { View, Image, StyleSheet } from "react-native";
 import { Icon, Text, Button, IconElement } from "@ui-kitten/components";
 
-const CalendarIcon = (props: any): IconElement => <Icon {...props} name="calendar-outline" />;
+const CalendarIcon = (props: any): IconElement => (
+    <Icon style={styles.calendar} name="calendar-outline" />
+);
 
 export const Header = () => {
     return (
         <View style={styles.header}>
             <View style={styles.container}>
-                <Button style={styles.calendar} appearance="ghost" accessoryLeft={CalendarIcon} />
+                <Button
+                    style={styles.button}
+                    appearance="ghost"
+                    accessoryLeft={CalendarIcon}
+                />
+
                 <Text style={styles.groupName}>ВБАб22о-1</Text>
                 <Text>28.08.2023-03.09.2023</Text>
             </View>
-            <Image style={styles.logo} source={require("./logo_rus_full_V.png")} />
+            <Image
+                style={styles.logo}
+                source={require("./logo_rus_full_V.png")}
+            />
         </View>
     );
 };
@@ -32,9 +42,13 @@ const styles = StyleSheet.create({
         height: 80,
         width: 90,
     },
+    button: {
+        width: 38,
+        height: 38,
+    },
     calendar: {
-        width: 40,
-        height: 40,
+        width: 32,
+        height: 32,
     },
     groupName: {
         fontWeight: "800",
