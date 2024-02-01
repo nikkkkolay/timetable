b707e225196a99e14fdfb9014daddefd326759be
Гагара Николай Федорович <ggn.f764@gmail.com>
2024-02-01 18:14:03 +0300

    исправил ошибку BottomSheet

--- screens/Timetable.tsx
+++ screens/Timetable.tsx
@@ -1,6 +1,8 @@
 import React, { useCallback, useEffect, useMemo, useRef } from "react";
 import { View, Text, StyleSheet } from "react-native";
-import BottomSheet from "@gorhom/bottom-sheet";
+import BottomSheet, { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
 import { Header, Calendar, Container } from "../components";
 
 export const Timetable = () => {
@@ -15,51 +17,52 @@ export const Timetable = () => {
         console.log("handleSheetChanges", index);
     }, []);
 
-    useEffect(() => {
-        const height = refView.current.getBoundingClientRect().height;
-        console.log(height, "height");
-    }, [refView]);
-
     return (
         <Container>
-            <View ref={refView}>
+            <View>
                 <Header />
                 <Calendar />
             </View>
-
-            <BottomSheet
-                ref={bottomSheetRef}
-                index={1}
-                snapPoints={snapPoints}
-                onChange={handleSheetChanges}
-                detached
-                enableContentPanningGesture
-                handleStyle={{
-                    backgroundColor: "#0064BE",
-                    borderTopStartRadius: 4,
-                    borderTopEndRadius: 4,
-                    shadowColor: "#000",
-                    shadowOffset: {
-                        width: 0,
-                        height: -4,
-                    },
-                    shadowOpacity: 0.4,
-                    shadowRadius: 3,
-                }}
-                handleIndicatorStyle={{ backgroundColor: "#ffffff" }}
-            >
-                <View style={styles.contentContainer}>
-                    <Text>Awesome 🎉</Text>
-                </View>
-            </BottomSheet>
+            <GestureHandlerRootView style={{ flex: 1 }}>
+                <BottomSheetModalProvider>
+                    <BottomSheet
+                        ref={bottomSheetRef}
+                        index={1}
+                        snapPoints={snapPoints}
+                        onChange={handleSheetChanges}
+                        handleStyle={{
+                            backgroundColor: "#008cfa",
+                            borderTopStartRadius: 4,
+                            borderTopEndRadius: 4,
+                            shadowColor: "#000",
+                            shadowOffset: {
+                                width: 0,
+                                height: -4,
+                            },
+                            shadowOpacity: 0.4,
+                            shadowRadius: 3,
+                        }}
+                        handleIndicatorStyle={{ backgroundColor: "#ffffff" }}
+                    >
+                        <View style={styles.contentContainer}>
+                            <Text>Awesome 🎉</Text>
+                        </View>
+                    </BottomSheet>
+                </BottomSheetModalProvider>
+            </GestureHandlerRootView>
         </Container>
     );
 };
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 24,
+        backgroundColor: "grey",
+    },
     contentContainer: {
         flex: 1,
         alignItems: "center",
-        backgroundColor: "#0064BE",
+        backgroundColor: "#008cfa",
     },
 });
