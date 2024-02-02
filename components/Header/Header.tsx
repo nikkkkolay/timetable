fe1d8d7496da4cf9ccc00f0bf67c23f0d7dfe47a
Гагара Николай Федорович <ggn.f764@gmail.com>
2024-02-02 18:12:30 +0300

    увыличил лого

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -30,8 +30,8 @@ const styles = StyleSheet.create({
     },
     logo: {
         resizeMode: "center",
-        height: 90,
-        width: 100,
+        height: 100,
+        width: 110,
     },
     button: {
         width: 38,
