b1cc62d6efae40d64a0b969de7833c7ddefdd017
Гагара Николай Федорович <gagaranf@mauniver.ru>
2024-06-25 16:09:07 +0300

    добавил кнопку загрузки

--- components/ListTimetable/ListTimetable.tsx
+++ components/ListTimetable/ListTimetable.tsx
@@ -1,13 +1,22 @@
 import React, { ReactElement } from "react";
 import { ListRenderItemInfo, StyleSheet, View } from "react-native";
-import { Card, List, Text } from "@ui-kitten/components";
+import { Card, List, Text, Button, Icon, IconElement } from "@ui-kitten/components";
 import { ScheduleTypes } from "../../store/useStore.types";
+import { useStore } from "../../store/useStore";
 
 export interface ITimetableProps {
     data: ScheduleTypes[];
 }
 
+const DownloadIcon = (): IconElement => <Icon style={styles.icon} name="download-outline" fill="#f2f5fa" />;
+
 export const ListTimetable = ({ data }: ITimetableProps): ReactElement => {
+    const { fetchingTimetable } = useStore((state) => state);
+
+    const downloadSchedule = () => {
+        console.log(123);
+    };
+
     const renderItemHeader = (info: ListRenderItemInfo<ScheduleTypes>): ReactElement => (
         <>
             {info.item.pair_first ? (
@@ -25,16 +34,36 @@ export const ListTimetable = ({ data }: ITimetableProps): ReactElement => {
 
     const renderItemFooter = (info: ListRenderItemInfo<ScheduleTypes>): ReactElement => <Text style={styles.row}>{info.item.room}</Text>;
 
-    const renderCard = (info: ListRenderItemInfo<ScheduleTypes>): ReactElement => (
-        <Card style={styles.card} status="basic" header={() => renderItemHeader(info)} footer={() => renderItemFooter(info)}>
-            <View style={styles.row}>
-                <Text style={styles.disciplines}>{info.item.disciplines}</Text>
-                <Text>{`— ${info.item.teacher}`}</Text>
-            </View>
-        </Card>
-    );
+    const renderCard = (info: ListRenderItemInfo<ScheduleTypes>): ReactElement => {
+        console.log(info.index);
 
-    return <List style={styles.container} contentContainerStyle={styles.contentContainer} data={data} renderItem={renderCard} />;
+        return (
+            <Card
+                style={info.index === data.length - 1 ? styles.cardNoMargin : styles.card}
+                status="basic"
+                header={() => renderItemHeader(info)}
+                footer={() => renderItemFooter(info)}
+            >
+                <View style={styles.row}>
+                    <Text style={styles.disciplines}>{info.item.disciplines}</Text>
+                    <Text>{`— ${info.item.teacher}`}</Text>
+                </View>
+            </Card>
+        );
+    };
+
+    return (
+        <>
+            <List style={styles.container} contentContainerStyle={styles.contentContainer} data={data} renderItem={renderCard} />
+            <Button
+                onPress={() => downloadSchedule()}
+                style={styles.downloadButton}
+                accessoryLeft={DownloadIcon}
+                status="warning"
+                disabled={fetchingTimetable}
+            />
+        </>
+    );
 };
 
 const styles = StyleSheet.create({
@@ -44,6 +73,9 @@ const styles = StyleSheet.create({
     },
     card: {
         marginBottom: 8,
+    },
+    cardNoMargin: {
+        marginBottom: 4,
     },
     row: {
         paddingVertical: 10,
@@ -71,5 +103,17 @@ const styles = StyleSheet.create({
     },
     contentContainer: {
         paddingHorizontal: 0,
+    },
+    downloadButton: {
+        position: "absolute",
+        right: 15,
+        bottom: 14,
+        width: 40,
+        height: 40,
+        borderWidth: 0,
+    },
+    icon: {
+        width: 26,
+        height: 26,
     },
 });
