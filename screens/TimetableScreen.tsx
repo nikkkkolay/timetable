7fb0697da65fe5118c0491d3a297cd431243df5f
Гагара Николай Федорович <gagaranf@mauniver.ru>
2024-08-12 16:00:13 +0300

    убрал получение данных по фокусу

--- screens/TimetableScreen.tsx
+++ screens/TimetableScreen.tsx
@@ -1,12 +1,9 @@
 import { useEffect } from "react";
-import { useAppState } from "@react-native-community/hooks";
 import { Header, Container, Timetable, Greeting, Error } from "../components";
 import { useStore } from "../store/useStore";
-import { format } from "@formkit/tempo";
 
 export const TimetableScreen = () => {
-    const { hasErrors, group, range, getCurrentSchedule, getAvailableDates, getSchedule, checkUpdateDate } = useStore((state) => state);
-    const currentAppState = useAppState();
+    const { hasErrors, group, getCurrentSchedule, getAvailableDates } = useStore((state) => state);
     const hasGroup = Object.keys(group).length !== 0;
 
     useEffect(() => {
@@ -18,23 +15,6 @@ export const TimetableScreen = () => {
         }
         setData();
     }, [hasErrors]);
-
-    // useEffect(() => {
-    //     const rangeStart = range.startDate && format(range.startDate, "YYYY-MM-DD");
-    //     const rangeEnd = range.endDate && format(range.endDate, "YYYY-MM-DD");
-    //     if (currentAppState === "active") {
-    //         async function getGroupId() {
-    //             await checkUpdateDate();
-    //             if (!hasErrors) {
-    //                 if (rangeStart && rangeEnd && group.uid) {
-    //                     getAvailableDates(group.uid);
-    //                     getSchedule(group.uid, rangeStart, rangeEnd);
-    //                 }
-    //             }
-    //         }
-    //         getGroupId();
-    //     }
-    // }, [currentAppState]);
 
     return (
         <Container>
